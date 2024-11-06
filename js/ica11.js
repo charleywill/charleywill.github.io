b08fbd997a2ec7b7598e229c55d1e04c3f0e5c12
charleywill <chwi1330@colorado.edu>
2024-11-06 15:11:56 -0700

    ica11 start

---
+++ js/ica11.js
@@ -0,0 +1,22 @@
+function tellFortune(numKids, partner, location, jobTitle){
+    let fortune = 'You will be a ' + jobTitle + ' in ' + location + ' and married to ' + partner + ' with ' + numKids + ' kids.';
+    console.log(fortune);
+}
+tellFortune(3, 'Mary', 'Germany', 'Engineer');
+tellFortune(5, 'George', 'Spain', 'Architect');
+tellFortune(0, 'Charlie', 'New York', 'Designer');
+
+
+function calculateDogAge(age){
+    let dogAge = age*7;
+    console.log('Your doggie is ' + dogAge + ' years old in dog years!');
+}
+calculateDogAge(3);
+calculateDogAge(1);
+calculateDogAge(8);
+
+
+function reverse(number){
+    number = number+"";
+    return n.split("").reverse().join("");
+}
