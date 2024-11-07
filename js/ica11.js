5757a00cde6a96c800ca566cfdb28af6a93b3c74
charleywill <chwi1330@colorado.edu>
2024-11-06 23:37:05 -0700

    finished

--- js/ica11.js
+++ js/ica11.js
@@ -1,5 +1,5 @@
 function tellFortune(numKids, partner, location, jobTitle){
-    let fortune = 'You will be a ' + jobTitle + ' in ' + location + ' and married to ' + partner + ' with ' + numKids + ' kids.';
+    let fortune = "You will be a " + jobTitle + " in " + location + " and married to " + partner + " with " + numKids + " kids.";
     console.log(fortune);
 }
 tellFortune(3, 'Mary', 'Germany', 'Engineer');
@@ -9,7 +9,7 @@ tellFortune(0, 'Charlie', 'New York', 'Designer');
 
 function calculateDogAge(age){
     let dogAge = age*7;
-    console.log('Your doggie is ' + dogAge + ' years old in dog years!');
+    console.log("Your doggie is " + dogAge + " years old in dog years!");
 }
 calculateDogAge(3);
 calculateDogAge(1);
@@ -23,5 +23,14 @@ function reverse(number){
 
 
 function alphabet_order(str){
-    return str.split('').sort().join('');
+    return str.split("").sort().join("");
+}
+
+function uppercase(str) {
+    let temp = str.split(" ");
+    let secTemp = [];
+    for (var x = 0; x < temp.length; x++) {
+      secTemp.push(temp[x].charAt(0).toUpperCase() + temp[x].slice(1));
+    }
+    return secTemp.join(" ");
 }
